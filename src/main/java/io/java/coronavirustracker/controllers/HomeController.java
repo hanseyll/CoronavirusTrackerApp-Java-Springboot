f0dd773ec4f84af8e99c927c66a9ae7f565f206e
hanseyll <vanhanseyll@gmail.com>
2023-01-12 18:26:46 -0500

    Showing data in html, start, start style process

--- src/main/java/io/java/coronavirustracker/controllers/HomeController.java
+++ src/main/java/io/java/coronavirustracker/controllers/HomeController.java
@@ -1,11 +1,16 @@
 package io.java.coronavirustracker.controllers;
 
+import io.java.coronavirustracker.services.coronavirusDataService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
+
+    coronavirusDataService coronavirusDataService;
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("locationStats",coronavirusDataService.getAllStats());
         return "home";
     }
 }
