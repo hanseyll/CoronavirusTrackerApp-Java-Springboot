81ae6e9fd1cc0833e2ae0860bd2471e25494e5c5
hanseyll <vanhanseyll@gmail.com>
2023-01-13 11:01:01 -0500

    data inserted from coronavirus cases

--- src/main/java/io/java/coronavirustracker/CoronavirusTrackerApplication.java
+++ src/main/java/io/java/coronavirustracker/CoronavirusTrackerApplication.java
@@ -2,9 +2,11 @@ package io.java.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
 @EnableScheduling
 public class CoronavirusTrackerApplication {
 
